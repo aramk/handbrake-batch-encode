6112489d777585fb6befcab2602c680310a64f5f
Aram Kocharyan <akarmenia@gmail.com>
2016-01-09 18:29:44 +1100

    Fix landscape, set date, add readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# Handbrake Batch Video Encoding
+
+This is a Ruby script which converts MOV, MP4, and M4V files shot with iOS devices into smaller files with lower quality and resolution, making them suitable for uploading and archiving personal videos. It is intended for use in OS X and other Unix environments.
+
+Typical 720p video files captured from an iPhone are reduced in file size by 8 times (e.g. 100MB -> 12.5MB) with the default settings (500p).
+
+## Usage
+
+	$ ./encode.rb <source_directory> [<target_directory>]
+	
+`target_directory` defaults to a sibling directory to `source_directory` with the "_encoded" suffix. `source_directory` is searched recursively for video files.
+
+## Features
+
+Constants in the Ruby script adjust the following features:
+
+* `VIDEO_HEIGHT` - The height in pixels (e.g. 500 (default), 720, 1080).
+* `CPU_RATIO` - CPU throttling to reduce overheating on very long encoding processes.
+* `FILE_DATE_META` - Sets created and modified dates in the output metadata to match the input.
+* `VIDEO_EXTENSIONS` - The file extensions on which to attempt conversion.
+
+## Installation
+
+Dependencies:
+
+* Ruby 2.0.0
+* HandBrakeCLI
+* exiftool
+* cpulimit
+
+### Installing with HomeBrew
+
+	brew install caskroom/cask/brew-cask
+	brew cask install handbrakecli
+	brew install exiftool
+	brew install cpulimit
+
+## License
+
+Released under the MIT License.
+
+This script is adapted from [ThomPatterson/Photos-Video-Archiver](https://github.com/ThomPatterson/Photos-Video-Archiver).
--- encode.rb
+++ encode.rb
@@ -5,6 +5,18 @@ require 'fileutils'
 require 'mini_exiftool'
 
 VIDEO_EXTENSIONS = ['mov', 'mp4', 'm4v']
+# e.g. Set to 720 or 1080 for better quality.
+VIDEO_HEIGHT = 500
+VIDEO_RATIO = 16.0/9
+
+# Ratio of CPU to use. Set to 1 to utilize full CPU capacity.
+CPU_RATIO = 0.5
+CPU_CORES = 4
+CPU_LIMIT = CPU_RATIO * 100 * CPU_CORES
+
+FILE_DATE_META = ['FileModifyDate', 'CreationDate']
+
+TARGET_DIR_SUFFIX = '_encoded'
 
 source_dir = ARGV[0]
 target_dir = ARGV[1]
@@ -14,7 +26,7 @@ unless source_dir and File.directory?(source_dir)
 end
 
 unless target_dir
-  target_dir = File.expand_path "#{ARGV[0]}/../converted"
+  target_dir = File.expand_path "#{ARGV[0]}/../#{File.basename(ARGV[0])}#{TARGET_DIR_SUFFIX}"
   FileUtils.mkdir_p(target_dir)
 end
 
@@ -27,18 +39,29 @@ def encode_video(source_file, dest_file, width, rotation=0, audio_bitrate=128, q
   # Rotate, 1 flips on x, 2 flips on y, 3 flips on both (equivalent of 180 degree rotation).
   if rotation == 90
     rotate_mode = 4
+    # Output width is now the source height.
+    width *= VIDEO_RATIO
   elsif rotation == 180
     rotate_mode = 3
   end
   if rotate_mode
     rotate_str = " --rotate=\"#{rotate_mode}\""
   end
-  `HandBrakeCLI -i "#{source_file}" -o "#{dest_file}" -e x264 -O -B #{audio_bitrate} -q #{quality} -w #{width} -x #{x264Advanced} #{rotate_str}`
+  pid = spawn "HandBrakeCLI -i '#{source_file}' -o '#{dest_file}' -e x264 -O -B #{audio_bitrate} -q #{quality} -X #{width} -x #{x264Advanced} #{rotate_str}"
+  # Limit the CPU during the conversion to reduce impact.
+  `cpulimit -p #{pid} -l #{CPU_LIMIT}`
 end
 
-def is_landscape(source_file)
-  width = `mdls -name kMDItemPixelWidth "#{source_file}" | grep -o '\d\{3,\}'`
-  height = `mdls -name kMDItemPixelHeight "#{source_file}" | grep -o '\d\{3,\}'`
+def is_landscape(file)
+  movie = MiniExiftool.new(file)
+  rotation = get_rotation(file)
+  if rotation == 90
+    width = movie.imageheight
+    height = movie.imagewidth
+  else
+    width = movie.imagewidth
+    height = movie.imageheight
+  end
   width > height
 end
 
@@ -47,13 +70,23 @@ def get_rotation(file)
   movie.rotation
 end
 
+def set_date(source_file, dest_file)
+  source = MiniExiftool.new(source_file)
+  dest = MiniExiftool.new(dest_file)
+  FILE_DATE_META.each do |property|
+    dest.copy_tags_from(source_file, property)
+  end
+  dest.save
+end
+
 file_queue = []
 
 Find.find(source_dir) do |name|
   next unless VIDEO_EXTENSIONS.include? File.extname(name).downcase.gsub(/^./, '')
   source_file = name
   dest_file = File.join target_dir, File.basename(name)
-  width = is_landscape(source_file) ? 1280 : 720
+  width = is_landscape(source_file) ? VIDEO_RATIO * VIDEO_HEIGHT : VIDEO_HEIGHT
   rotation = get_rotation(source_file)
   encode_video(source_file, dest_file, width, rotation)
+  set_date(source_file, dest_file)
 end
